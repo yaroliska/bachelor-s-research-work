96bbdc975fbc4f409b0589acf96e6e81525c8d98
yaroslavnakozlova <kozlovaiaroslavna@gmail.com>
2019-03-18 12:55:30 +0400

    iHandleMouseUp,drawLine, getCanvas, getICanvas,is ValidPaths, drawDots, functions

--- front/src/components/main/canvas_editor.vue
+++ front/src/components/main/canvas_editor.vue
@@ -24,6 +24,8 @@
         step: 20,
         canvas:{
         },
+        icanvas:{
+        },
         listOfPaths:[],
         possiblePath:{
           number:'2',
@@ -37,6 +39,7 @@
       }
     },
     methods: {
+      //функции отлова мыши
       iHandleMouseMove:function(event){
         this.mouse.current = {
           x: event.offsetX,
@@ -47,6 +50,7 @@
       iHandleMouseDown:function (event) {
         this.mouse.down = true;
         //добавить привязку к состоянию редактора
+        //функция подсветки рисуемой линии
         this.possiblePath.xbegin = this.align(this.mouse.current.x, this.step);
         this.possiblePath.ybegin = this.align(this.mouse.current.y, this.step);
         //console.log('Нажато в точке '+ this.mouse.current.x +' ' +this.mouse.current.y);
@@ -54,20 +58,92 @@
       },
       iHandleMouseUp:function (event) {
         //добавить привязку к состоянию редактора
-       /* this.possiblePath.xend = this.align(event.offsetX, this.step);
-        possiblePath.y2 = align(e.offsetY, step);
-        drawLine(ctx,possiblePath.x1,possiblePath.y1, possiblePath.x2, possiblePath.y2);
-        //функция отправки в базу данных новой линии и прием из базы сгенерированного id для нового пути
-        console.log(pathsArray);
-        pathsArray.push(Object.assign({},possiblePath));
-        console.log(pathsArray);*/
+        this.possiblePath.xend = this.align(this.mouse.current.x, this.step);
+        this.possiblePath.yend = this.align(this.mouse.current.y, this.step);
+        this.drawLine(this.icanvas.ctx,this.possiblePath.xbegin, this.possiblePath.ybegin, this.possiblePath.xend, this.possiblePath.yend);
+        //функция отправки в базу данных новой линии и получения из базы сгенерированного id для нового пути
+        if(this.isValidPath(this.possiblePath)) {
+          this.listOfPaths.push(Object.assign({}, this.possiblePath));
+          console.log(this.listOfPaths);
+        }
+        else console.log('Это было одинарное нажатие, путь записать нельзя');
       },
+
+      //основные функции
+      //находим ближайшую усредненную точку
       align:function (val, step) {
         return val -(val%step)+step/2;
       },
-      getCanvas:function () {
+
+      //функция рисовании линии по двум точкам
+      drawLine:function(ctx,x1,y1,x2,y2){
+        ctx.beginPath();
+        ctx.moveTo(x1,y1);
+        ctx.lineTo(x2,y2);
+        ctx.stroke();
+      },
+
+      //функция отрисовки всех путей, лежащих в listOfPaths
+      drawPaths:function(ctx){
+        for (let value of this.listOfPaths) {
+          console.log(value);
+          this.drawLine(ctx,value.xbegin,value.ybegin,value.xend,value.yend);
+        }
+      },
+
+
+
+      //функция рисования точек на канвасе
+      drawDots:function(canvas){
+        for (let i=this.step/2; i<canvas.width; i+=this.step){
+          for (let j=this.step/2; j<canvas.height; j+=this.step){
+            canvas.ctx.fillRect(i,j,1,1)
+          }
+        }
+      },
+
+      //функция обработки одинарного нажатия при записи пути
+      isValidPath:function(path){
+        if ((path.xbegin===path.xend)&&(path.ybegin ===path.yend))
+          return false;
+        else return true;
+      },
+
+
+
+
+      //функции работающие с бд
+
+      //функция отправки в базу данных новой линии и получения из базы сгенерированного id для нового пути
+      setPath:function(){
+
+      },
+
+      //функция получения всех путей из базы данных и их записи во фронт
+      getPaths:function(){
+        //временная функция создания тестовых путей
+      },
+
+      //функции возвращающие canvas
+      getCanvas:function(){
+        this.canvas.width = document.getElementById("canvas").width;
+        this.canvas.height =  document.getElementById("canvas").height;
+        this.canvas.ctx =  document.getElementById("canvas").getContext("2d");
+      },
+      getICanvas:function () {
+        this.icanvas.width = document.getElementById("interactive-canvas").width;
+        this.icanvas.height =  document.getElementById("interactive-canvas").height;
+        this.icanvas.ctx =  document.getElementById("interactive-canvas").getContext("2d");
+      },
+
+
+
+      //тестовые функции
+      createListOfPaths:function(){
 
       }
+
+
      /* align:function(val, st){
         return val - (val%st) + st/2;
       },
@@ -133,15 +209,26 @@
     },
     mounted: function () {
       //получаем данные с канваса при загрузке
-      this.canvas.width = document.getElementById("canvas").width;
-      this.canvas.height =  document.getElementById("canvas").height;
-      this.canvas.ctx =  document.getElementById("canvas").getContext("2d");
-     // this.drawDots();
+      this.getCanvas();
+      this.getICanvas();
+      this.drawDots(this.canvas);
       //this.drawPaths();
     }
   }
 </script>
 
 <style scoped>
-
+  #canvas{
+    position: absolute;
+    top: 0;
+    left: 0;
+    border:1px solid #d3d3d3;
+  }
+  #interactive-canvas{
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index:2;
+    border:1px solid #d3d3d3;
+  }
 </style>
